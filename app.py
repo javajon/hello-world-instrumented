da90be927b55d9ffc15fa5d7e3a2115d8e7fcec0
Jonathan Johnson <jjohnson@coreinformatics.com>
2018-04-05 08:38:38 -0400

    experiment app change

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ hostname = socket.gethostname()
 @app.route('/')
 def index():
     # Call Greeter Service
-    timestamp = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))	
+    timestamp = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     app.logger.debug("Starting at: " + timestamp)
-    
+
     # Check if a sleep was passed in as a parameter
     sleep = request.args.get("sleep")
     if sleep is not None:
@@ -31,11 +31,11 @@ def index():
       # Force a sleep.
       # Comment this for demo purposes
       app.logger.debug("Forcing a sleep")
-      # time.sleep(2)
+      time.sleep(2)
 
     timestamp2 = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     app.logger.debug("Finished at: " + timestamp2)
-    return timestamp2 + " " + hostname + " Hello, World!!\n"
+    return timestamp2 + " " + hostname + " Hello, World!! JJ-test\n"
 
 if __name__ == '__main__':
     monitor(app, port=8000)
